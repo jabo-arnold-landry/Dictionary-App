bdaaba07196e46dc93fc6b76868627f340de2123
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-17 16:09:36 +0200

    initial setup for the TS file
    in this file I made some types for repeating pattern of the same pattern and also I have made some essential interfaces to handle data structuring and ensures data consistency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+type DefinitionArray = string[];
+
+interface Meaning {
+  difinition: DefinitionArray;
+  partOfSpeech: string;
+  synonyms: DefinitionArray;
+}
+
+interface VerbOrNoun {
+  certainType: Meaning;
+}
+
+type DictionaryFilteredData = VerbOrNoun[];
+
+async function getTransilation<T = any>(word: string): Promise<T> {
+  const transilationResponse = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+  );
+  const transilationedTextData = await transilationResponse.json();
+  console.log(transilationedTextData);
+  return transilationedTextData as T;
+}
+
+getTransilation("keyboard");
