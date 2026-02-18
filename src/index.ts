f59c3f507980375f04650ba7216416732587d89c
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-18 15:37:58 +0200

    refactor: Romove two useless interface and instead used one that generalize the whole structure of the data

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
 type DefinitionArray = string[];
 type Str = string;
 
-type MeaningStructure = {
+interface MeaningStructure {
   difinition: DefinitionArray;
-  partOfSpeech: string;
+  partOfSpeech: Str;
   synonyms: DefinitionArray;
   antonyms?: DefinitionArray;
 };
 
-interface Meaning {
-  noun: MeaningStructure;
-}
-
-interface VerbMeaning {
-  verb: MeaningStructure;
-}
-const arr: any[] = [];
-
-async function getTransilation<T = any>(word: string): Promise<T> {
+async function getTransilation<T = any>(word: Str): Promise<T> {
   const transilationResponse = await fetch(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
   );
@@ -30,10 +21,9 @@ const formElement = document.querySelector("form");
 formElement?.addEventListener("submit", async (e) => {
   e.preventDefault();
   const textElement = formElement.querySelector("input");
-  const meaningData = await getTransilation(textElement?.value as string);
-  const nounMeaning: Meaning = { noun: meaningData[0].meanings[0] };
-  const verbMeaning: VerbMeaning = { verb: meaningData[0].meanings[1] };
-  arr.push(nounMeaning, verbMeaning);
+  const meaningData = await getTransilation(textElement?.value as Str);
+  const nounMeaning: MeaningStructure =  meaningData[0].meanings[0] 
+  const verbMeaning: MeaningStructure =  meaningData[0].meanings[1] 
   let audio: Str = meaningData[0].phonetics[0].audio;
   let srcs = meaningData[0].sourceUrls[0];
 });
