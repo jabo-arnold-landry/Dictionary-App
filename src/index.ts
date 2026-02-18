dbb6e10b4176be41589f08afddcf4b6ab2a450f3
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-18 15:39:12 +0200

    feat: Add a new type to extract only the data we want from the meaing of words object which is the <Pick> utility type and created a function that will populate the dictionary meaning to the screen

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ interface MeaningStructure {
   partOfSpeech: Str;
   synonyms: DefinitionArray;
   antonyms?: DefinitionArray;
-};
+}
 
 async function getTransilation<T = any>(word: Str): Promise<T> {
   const transilationResponse = await fetch(
@@ -22,8 +22,18 @@ formElement?.addEventListener("submit", async (e) => {
   e.preventDefault();
   const textElement = formElement.querySelector("input");
   const meaningData = await getTransilation(textElement?.value as Str);
-  const nounMeaning: MeaningStructure =  meaningData[0].meanings[0] 
-  const verbMeaning: MeaningStructure =  meaningData[0].meanings[1] 
+  const nounMeaning: MeaningStructure = meaningData[0].meanings[0];
+  const verbMeaning: MeaningStructure = meaningData[0].meanings[1];
   let audio: Str = meaningData[0].phonetics[0].audio;
   let srcs = meaningData[0].sourceUrls[0];
 });
+type PickOne = Pick<
+  MeaningStructure,
+  "difinition" | "partOfSpeech" | "synonyms"
+>;
+
+function nounPopulation(obj: PickOne, className: string): void {
+  const sectionToGet = document.querySelector(`.${className}`);
+  const { difinition, partOfSpeech, synonyms } = obj;
+  console.log(obj);
+}
